b178f68499ba98c6fcf352179200ebc04bb7a9da
libyano93ly <younus93ly@gmail.com>
2022-08-01 11:03:44 +0200

    Initial Commit

---
+++ Finalizer-Destructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Finalizer_Destructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.Name = "Younis Sultan";
+            Console.ReadKey();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person()//Constructor
+        {
+            Console.WriteLine("This is Person Constructor");
+        }
+
+        ~Person()//Destructor
+        {
+            Console.WriteLine("This is Person Destructor");
+        }
+    }
+}
